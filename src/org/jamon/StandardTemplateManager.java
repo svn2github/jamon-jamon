2139f40aca1f240e3cf2c1e5e5c6a63153c466c1
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-06-10 06:20:49 +0000

    Hacked-together (but functional!) template manager.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@26 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- src/org/jamon/StandardTemplateManager.java
+++ src/org/jamon/StandardTemplateManager.java
@@ -3,6 +3,7 @@ package org.modusponens.jtt;
 import java.io.File;
 import java.io.Writer;
 import java.io.PushbackReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -17,8 +18,8 @@ public class StandardTemplateManager
     implements TemplateManager
 {
 
-    private String m_templateSourceDir;
-    private String m_workDir;
+    private String m_templateSourceDir = "testdata";
+    private String m_workDir = "work";
 
     public StandardTemplateManager()
     {
@@ -59,20 +60,20 @@ public class StandardTemplateManager
         String dir, name;
         if (i >= 0)
         {
-            dir = p_path.substring(0,i-1);
-            name = p_path.substring(i);
+            dir = p_path.substring(1,i).replace('/','.');
+            new File(m_workDir + p_path).mkdirs();
+            name = p_path.substring(i+1);
         }
         else
         {
             dir = "";
             name = p_path;
         }
-        Phase2Generator g2 =
-            new Phase2Generator(new FileWriter(getJavaFileName(p_path)),
-                                dir,
-                                name);
+        FileWriter w = new FileWriter(getJavaFileName(p_path));
+        Phase2Generator g2 = new Phase2Generator(w,dir,name);
         parser.parse().apply(g2);
         g2.generateClassSource();
+        w.close();
         return System.currentTimeMillis();
     }
 
@@ -107,22 +108,101 @@ public class StandardTemplateManager
         }
     }
 
-    private Class loadAndResolveClass(String p_path)
+    private class Loader
+        extends ClassLoader
+    {
+        private byte [] readBytesForClass(String p_name)
+            throws IOException
+        {
+            FileInputStream s =
+                new FileInputStream(getClassFileNameForClass(p_name));
+            final byte [] buf = new byte[1024];
+            byte [] bytes = new byte[0];
+            while (true)
+            {
+                int i = s.read(buf);
+                if (i <= 0)
+                {
+                    break;
+                }
+                byte [] newbytes = new byte[bytes.length + i];
+                System.arraycopy(bytes,0,newbytes,0,bytes.length);
+                System.arraycopy(buf,0,newbytes,bytes.length,i);
+                bytes = newbytes;
+            }
+            return bytes;
+        }
+
+        private String getClassFileNameForClass(String p_name)
+        {
+            return m_workDir + "/" + p_name.replace('.','/') + "Impl.class";
+        }
+
+        protected Class findClass(String p_name)
+            throws ClassNotFoundException
+        {
+            try
+            {
+                byte [] bytecode = readBytesForClass(p_name);
+                return defineClass(p_name+"Impl",bytecode,0,bytecode.length);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+                throw new ClassNotFoundException();
+            }
+        }
+
+        public Class load(String p_name, boolean p_resolve)
+            throws ClassNotFoundException
+        {
+            Class c = findClass(p_name);
+            resolveClass(c);
+            return c;
+
+        }
+    }
+
+    private final Loader m_loader = new Loader();
+
+    private String getClassName(String p_path)
     {
         // FIXME
-        return null;
+        return p_path.replace('/','.').substring(1);
+    }
+
+    private Class loadAndResolveClass(String p_path)
+        throws ClassNotFoundException
+    {
+        return m_loader.load(getClassName(p_path),true);
     }
 
     private void compile(String p_path)
         throws IOException
     {
         // FIXME
+        String cmdline = "/usr/local/java/bin/javac -classpath " + m_workDir + ":" + System.getProperty("java.class.path") + " " + getJavaFileName(p_path);
+        System.err.println("Compiling " + p_path + " with " + cmdline);
+        Process p = Runtime.getRuntime().exec(cmdline);
+        while (true)
+        {
+            try
+            {
+                p.waitFor();
+                break;
+            }
+            catch (InterruptedException e)
+            {
+            }
+        }
+
     }
 
     private Class getClass(String p_path)
         throws IOException,
                ParserException,
-               LexerException
+               LexerException,
+               ClassNotFoundException
     {
         File cf = new File(getClassFileName(p_path));
         if (cf.lastModified() < getLastModifiedJava(p_path))
@@ -135,46 +215,15 @@ public class StandardTemplateManager
 
     public Template getInstance(String p_path, Writer p_writer)
     {
-        // this method SHOULD:
-        //   use its own class loader (!)
-        //   check that the class exists. if it doesn't,
-        //     to generate it.
-        //   verify that the class is up to date. if it isn't
-        //     generate it.
         try
         {
-            Class c = Class.forName(p_path + "Impl");
+            Class c = getClass(p_path);
             Constructor con =
                 c.getConstructor(new Class [] { Writer.class,
                                                 TemplateManager.class });
             return (Template) con.newInstance(new Object [] { p_writer, this });
         }
-        catch (ClassNotFoundException e)
-        {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
-        catch (NoSuchMethodException e)
-        {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
-        catch (InstantiationException e)
-        {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
-        catch (IllegalAccessException e)
-        {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
-        catch (IllegalArgumentException e)
-        {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage());
-        }
-        catch (InvocationTargetException e)
+        catch (Exception e)
         {
             e.printStackTrace();
             throw new RuntimeException(e.getMessage());
