46358ca3386e3dd084a6ffd5ec4c120e282f5fd5
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-06-14 01:39:47 +0000

    More refactoring.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@68 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- src/org/jamon/StandardTemplateManager.java
+++ src/org/jamon/StandardTemplateManager.java
@@ -6,7 +6,6 @@ import java.io.PushbackReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URLClassLoader;
@@ -19,22 +18,26 @@ import org.modusponens.jtt.lexer.LexerException;
 public class StandardTemplateManager
     implements TemplateManager
 {
+    private static final String PS = System.getProperty("path.separator");
+    private static final String FS = System.getProperty("file.separator");
 
     private String m_templateSourceDir = "testdata";
     private String m_workDir = "work";
     private String m_javac =
-        System.getProperty("java.home") + "/../bin/javac";
+        System.getProperty("java.home") + FS + ".." + FS + "bin" + FS +"javac";
     private boolean m_includeRtJar = false;
     private String m_classpath = "";
     private ClassLoader m_classLoader = ClassLoader.getSystemClassLoader();
     private String m_packagePrefix = "";
+    private JavaCompiler m_javaCompiler;
+    private final ClassLoader m_loader;
 
     public StandardTemplateManager(ClassLoader p_parentLoader)
         throws IOException
     {
         m_loader = new URLClassLoader(new URL []
                                       {
-                                          new URL("file:" + m_workDir + "/")
+                                          new URL("file:" + m_workDir + FS)
                                       },
                                       p_parentLoader);
     }
@@ -48,6 +51,7 @@ public class StandardTemplateManager
     public void setWorkDir(String p_workDir)
     {
         m_workDir = p_workDir;
+        m_javaCompiler = null;
     }
 
     public void setTemplateSourceDir(String p_dir)
@@ -58,24 +62,75 @@ public class StandardTemplateManager
     public void setJavaCompiler(String p_javac)
     {
         m_javac = p_javac;
+        m_javaCompiler = null;
     }
 
     public void setJavaCompilerNeedsRtJar(boolean p_includeRtJar)
     {
         m_includeRtJar = p_includeRtJar;
+        m_javaCompiler = null;
     }
 
     public void setClasspath(String p_classpath)
     {
         m_classpath = p_classpath;
+        m_javaCompiler = null;
     }
 
     public void setPackagePrefix(String p_packagePrefix)
     {
         m_packagePrefix = p_packagePrefix;
+        m_javaCompiler = null;
     }
 
-    private long createJavaFile(String p_path)
+    private String getJavaFileName(String p_path)
+    {
+        return m_workDir + p_path + "Impl.java";
+    }
+
+    private String getJavaClassFileName(String p_path)
+    {
+        return m_workDir + p_path + "Impl.class";
+    }
+
+    private String getTemplateFileName(String p_path)
+    {
+        return m_templateSourceDir + p_path;
+    }
+
+    private String getClassName(String p_path)
+    {
+        return PathUtils.pathToClassName(p_path) + "Impl";
+    }
+
+    private Class loadAndResolveClass(String p_path)
+        throws ClassNotFoundException
+    {
+        // FIXME: need to check that it still implements the interface
+        return m_loader.loadClass(getClassName(p_path));
+    }
+
+    private String getClassPath()
+    {
+        String cp = m_classpath != null ? (m_classpath + PS) : "";
+        cp = m_workDir + PS + cp + System.getProperty("java.class.path");
+        if (m_includeRtJar)
+        {
+            cp += PS + System.getProperty("java.home") + FS + "lib" + FS +"rt.jar";
+        }
+        return cp;
+    }
+
+    private JavaCompiler getJavaCompiler()
+    {
+        if (m_javaCompiler == null)
+        {
+            m_javaCompiler = new JavaCompiler(m_javac, getClassPath());
+        }
+        return m_javaCompiler;
+    }
+
+    private void createJavaFile(String p_path)
         throws IOException,
                ParserException,
                LexerException
@@ -84,12 +139,12 @@ public class StandardTemplateManager
             new Parser(new Lexer(new PushbackReader
                                  (new FileReader(getTemplateFileName(p_path)),
                                   1024)));
-        int i = p_path.lastIndexOf('/');
+        int i = p_path.lastIndexOf(FS);
         String dir, name;
         if (i >= 1)
         {
             new File(m_workDir + p_path.substring(0,i)).mkdirs();
-            dir = p_path.substring(1,i).replace('/','.');
+            dir = PathUtils.pathToClassName(p_path.substring(1,i));
             name = p_path.substring(i+1);
         }
         else if (i == 0)
@@ -116,78 +171,39 @@ public class StandardTemplateManager
             new File(getJavaFileName(p_path)).delete();
             throw e;
         }
-        return System.currentTimeMillis();
     }
 
-    private String getJavaFileName(String p_path)
+    private boolean isJavaFileUpToDate(String p_path)
+        throws IOException
     {
-        return m_workDir + p_path + "Impl.java";
-    }
-
-    private String getImplementationClassFileName(String p_path)
-    {
-        return m_workDir + p_path + "Impl.class";
-    }
-
-    private String getTemplateFileName(String p_path)
-    {
-        return m_templateSourceDir + p_path;
-    }
-
-    private long getLastModifiedJava(String p_path)
-        throws IOException,
-               ParserException,
-               LexerException
-    {
-        File f = new File(getJavaFileName(p_path));
-        if (! f.exists())
-        {
-            return createJavaFile(p_path);
-        }
-        else
+        File jf = new File(getJavaFileName(p_path));
+        if (jf.exists())
         {
             File tf = new File(getTemplateFileName(p_path));
             if (! tf.exists() )
             {
                 throw new JttException("Template file "
                                        + p_path
-                                       + " missing!");
+                                       + " missing?!");
             }
-            if ( tf.lastModified() > f.lastModified() )
-            {
-                return createJavaFile(p_path);
-            }
-            else
-            {
-                return f.lastModified();
-            }
+            return tf.lastModified() < jf.lastModified();
         }
-    }
-
-    private final ClassLoader m_loader;
-
-    private String getClassName(String p_path)
-    {
-        // FIXME
-        return p_path.replace('/','.').substring(1);
-    }
-
-    private Class loadAndResolveClass(String p_path)
-        throws ClassNotFoundException
-    {
-        // FIXME: need to check that it still implements the interface
-        return m_loader.loadClass(getClassName(p_path)+"Impl");
-    }
-
-    private String getClassPath()
-    {
-        String cp = m_classpath != null ? (m_classpath + ':') : "";
-        cp = m_workDir + ":" + cp + System.getProperty("java.class.path");
-        if (m_includeRtJar)
+        else
         {
-            cp += ':' + System.getProperty("java.home") + "/lib/rt.jar";
+            return false;
         }
-        return cp;
+    }
+
+    private boolean isClassFileUpToDate(String p_path)
+        throws IOException
+    {
+        File jf = new File(getJavaFileName(p_path));
+        if (!jf.exists())
+        {
+            throw new IOException("java file disappeared!?");
+        }
+        File cf = new File(getJavaClassFileName(p_path));
+        return jf.lastModified() < cf.lastModified();
     }
 
     private Class getImplementationClass(String p_path)
@@ -196,12 +212,16 @@ public class StandardTemplateManager
                LexerException,
                ClassNotFoundException
     {
-        File cf = new File(getImplementationClassFileName(p_path));
-        if (cf.lastModified() < getLastModifiedJava(p_path))
+        if (! isJavaFileUpToDate(p_path))
         {
-            new JavaCompiler(m_javac, getClassPath())
-                .compile(getJavaFileName(p_path));
+            createJavaFile(p_path);
         }
+
+        if (! isClassFileUpToDate(p_path) )
+        {
+            getJavaCompiler().compile(getJavaFileName(p_path));
+        }
+
         return loadAndResolveClass(p_path);
     }
 
