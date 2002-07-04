145e9ba7cdf6ecfd902473d8177f5f1071c67763
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-07-04 01:39:57 +0000

    Javadoc.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@108 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/src/org/jamon/TemplateResolver.java
+++ core/src/org/jamon/TemplateResolver.java
@@ -1,15 +1,55 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Jamon code, released ??.
+ *
+ * The Initial Developer of the Original Code is Jay Sachs.  Portions
+ * created by Jay Sachs are Copyright (C) 2002 Jay Sachs.  All Rights
+ * Reserved.
+ *
+ * Contributor(s):
+ */
+
 package org.modusponens.jtt;
+
+/**
+ * A <code>TemplateResolver</code> provides methods to translate
+ * template paths to the corresponding interface class name,
+ * implementation class name, and the package name for each.
+ */
 
 public class TemplateResolver
 {
     private static final String FS = System.getProperty("file.separator");
 
+    /**
+     * Construct a <code>TemplateResolver</code> specifying the prefix
+     * to prepend to the package names for both interface and
+     * implementation template classes.
+     *
+     * @param p_packagePrefix the package prefix
+     */
     public TemplateResolver(String p_packagePrefix)
     {
         m_packagePrefix = p_packagePrefix;
     }
 
-    private final String m_packagePrefix;
+    /**
+     * Given a template path, return the name of the interface class
+     * for that template.
+     *
+     * @param p_path the template path
+     *
+     * @return the name of the interface class
+     */
 
     public String getIntfClassName(final String p_path)
     {
@@ -17,10 +57,29 @@ public class TemplateResolver
         return i < 0 ? p_path : p_path.substring(i+1);
     }
 
+    /**
+     * Given a template path, return the name of the implementation
+     * class for that template.
+     *
+     * @param p_path the template path
+     *
+     * @return the name of the implementation class
+     */
+
     public String getImplClassName(final String p_path)
     {
         return getIntfClassName(p_path) + "Impl";
     }
+
+
+    /**
+     * Given a template path, return the name of the package in which
+     * the interface class for that template lives.
+     *
+     * @param p_path the template path
+     *
+     * @return the name of the interface class package
+     */
 
     public String getIntfPackageName(final String p_path)
     {
@@ -41,8 +100,21 @@ public class TemplateResolver
         return pkg.toString();
     }
 
+
+    /**
+     * Given a template path, return the name of the package in which
+     * the implementation class for that template lives.
+     *
+     * @param p_path the template path
+     *
+     * @return the name of the implementation class package
+     */
+
     public String getImplPackageName(final String p_path)
     {
         return getIntfPackageName(p_path);
     }
+
+    /** the package prefix */
+    private final String m_packagePrefix;
 }
