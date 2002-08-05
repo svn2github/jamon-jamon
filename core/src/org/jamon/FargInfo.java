2c0d0804a808dda09264f0b470d4c2857061cc90
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-08-05 10:49:50 +0000

    Initial version sketch.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@130 6afd1a24-8710-0410-9bc4-c69fca332c4c

---
+++ core/src/org/jamon/FargInfo.java
@@ -0,0 +1,25 @@
+package org.modusponens.jtt;
+
+import java.util.Iterator;
+
+class FargInfo
+{
+    public FargInfo()
+    {
+    }
+
+    public Iterator getArgumentNames()
+    {
+        return null;
+    }
+
+    public String getArgumentType(String p_name)
+    {
+        return null;
+    }
+
+    public String getFargInterfaceName()
+    {
+        return null;
+    }
+}
