61947c025ae0331eb4fb2576d99ee4524c6fb9f1
luis <luis@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-08-20 20:49:45 +0000

    Have the template render onto a StringWriter and then check output against a RE.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@156 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- integration/src/test/jamon/Test1.java
+++ integration/src/test/jamon/Test1.java
@@ -1,30 +1,45 @@
 package test.jamon;
 
+import gnu.regexp.RE;
+import gnu.regexp.RESyntax;
+
+import java.io.Writer;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+
+import org.jamon.StandardTemplateManager;
+import org.jamon.StringUtils;
+
 import foo.bar.test.jamon.TestTemplate;
-import java.io.*;
-import java.math.*;
-import org.jamon.*;
 
 public class Test1
 {
+
     public static void main(String [] args)
+        throws Exception
     {
-        try
+        Writer w = new StringWriter();
+        StandardTemplateManager m =
+            new StandardTemplateManager("templates",
+                                        "build/work");
+        m.setPackagePrefix("foo.bar.");
+        TestTemplate.Factory f = new TestTemplate.Factory(m);
+        TestTemplate t = f.getInstance(w);
+        t.setX(57);
+        t.render(new BigDecimal("34.5324"));
+        w.flush();
+
+        // Now perform some tests
+        RE re = new RE(".*An external template with a parameterized fragment parameter \\(farg\\)" +
+                       "\\s*" +
+                       "i is 3 and s is yes." +
+                       "\\s*" +
+                       "i is 7 and s is no.*",
+                       RE.REG_DOT_NEWLINE);
+        if (!re.isMatch(w))
         {
-            Writer w = new OutputStreamWriter(System.out);
-            StandardTemplateManager m =
-                new StandardTemplateManager("templates",
-                                            "build/work");
-            m.setPackagePrefix("foo.bar.");
-            TestTemplate.Factory f = new TestTemplate.Factory(m);
-            TestTemplate t = f.getInstance(w);
-            t.setX(57);
-            t.render(new BigDecimal("34.5324"));
-            w.flush();
-        }
-        catch (Throwable t)
-        {
-            t.printStackTrace();
+            throw new Exception("Failed to match " + re);
         }
     }
+
 }
