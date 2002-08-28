6eef9294bb3419635a0b4b529df84504281b502c
luis <luis@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-08-28 16:23:58 +0000

    - Initial version of unit test and template - Add JavaEscape to list of templates that will be compiled
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@160 6afd1a24-8710-0410-9bc4-c69fca332c4c

---
+++ integration/src/org/jamon/integration/JavaTest.java
@@ -0,0 +1,45 @@
+package org.jamon.integration;
+
+import java.io.Writer;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+
+import junit.framework.TestCase;
+
+import org.jamon.StandardTemplateManager;
+import org.jamon.StringUtils;
+
+import foo.bar.test.jamon.JavaEscape;
+
+/************************************************************************************
+ *
+ * Test Jamon's java escapes.  See "Jamon User's Guide", section 2.
+ *
+ ************************************************************************************/
+
+public class JavaTest
+    extends TestCase
+{
+
+    public JavaTest(String p_name)
+    {
+        super(p_name);
+    }
+
+
+    public void testExercise()
+        throws Exception
+    {
+        Writer w = new StringWriter();
+        StandardTemplateManager m =
+            new StandardTemplateManager("templates",
+                                        "build/work");
+        m.setPackagePrefix("foo.bar.");
+        JavaEscape.Factory f = new JavaEscape.Factory(m);
+        JavaEscape t = f.getInstance(w);
+        t.render();
+        w.flush();
+        assertEquals("0\n1\n2\n", w.toString());
+    }
+
+}
