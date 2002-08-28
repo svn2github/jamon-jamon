6234961baf891f833f817b962953daccf93b4fee
luis <luis@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-08-28 21:26:18 +0000

    Initial version.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@167 6afd1a24-8710-0410-9bc4-c69fca332c4c

---
+++ integration/src/org/jamon/integration/FragmentTest.java
@@ -0,0 +1,41 @@
+package org.jamon.integration;
+
+import java.io.Writer;
+import java.io.StringWriter;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.jamon.StandardTemplateManager;
+import org.jamon.StringUtils;
+import org.jamon.JttException;
+
+import foo.bar.test.jamon.Fragment;
+
+/************************************************************************************
+ *
+ * Test Jamon's template fragments.  See "Jamon User's Guide", section 8.
+ *
+ ************************************************************************************/
+
+public class FragmentTest
+    extends TestCase
+{
+
+    public void testExercise()
+        throws IOException, 
+               JttException
+    {
+        Writer w = new StringWriter();
+        StandardTemplateManager m =
+            new StandardTemplateManager("templates",
+                                        "build/work");
+        m.setPackagePrefix("foo.bar.");
+        Fragment.Factory f = new Fragment.Factory(m);
+        Fragment t = f.getInstance(w);
+        t.render(1);
+        w.flush();
+        assertEquals("1(2)1", w.toString());
+    }
+
+}
---
+++ integration/src/org/jamon/integration/ParametrizedFragmentTest.java
@@ -0,0 +1,42 @@
+package org.jamon.integration;
+
+import java.io.Writer;
+import java.io.StringWriter;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.jamon.StandardTemplateManager;
+import org.jamon.StringUtils;
+import org.jamon.JttException;
+
+import foo.bar.test.jamon.ParametrizedFragment;
+
+/************************************************************************************
+ *
+ * Test Jamon's parametrized template fragments.  See "Jamon User's Guide", 
+ * section 9.
+ *
+ ************************************************************************************/
+
+public class ParametrizedFragmentTest
+    extends TestCase
+{
+
+    public void testExercise()
+        throws IOException, 
+               JttException
+    {
+        Writer w = new StringWriter();
+        StandardTemplateManager m =
+            new StandardTemplateManager("templates",
+                                        "build/work");
+        m.setPackagePrefix("foo.bar.");
+        ParametrizedFragment.Factory f = new ParametrizedFragment.Factory(m);
+        ParametrizedFragment t = f.getInstance(w);
+        t.render(new int[] { -2, 0, 15 });
+        w.flush();
+        assertEquals("-0+", w.toString());
+    }
+
+}
