78c80e1bc3e035f29bc92594a59b520b26b4b191
luis <luis@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-08-29 16:43:12 +0000

    Expose the absolute filenames that interfaces will be written to in order to facilitate writing of an Ant task.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@171 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/src/org/jamon/TemplateGenerator.java
+++ core/src/org/jamon/TemplateGenerator.java
@@ -17,11 +17,31 @@ public class TemplateGenerator
     private static final String FILESEP =
         System.getProperty("file.separator");
 
+
     private static void generateInterface(File p_destdir,
                                           TemplateDescriber p_describer,
                                           TemplateResolver p_resolver,
                                           String p_pkgPrefix,
                                           String p_filename)
+        throws IOException,
+               ParserException,
+               LexerException
+    {
+        generateInterface(p_destdir,
+                          p_describer,
+                          p_resolver,
+                          p_pkgPrefix,
+                          p_filename,
+                          p_filename);
+    }
+
+
+    private static void generateInterface(File p_destdir,
+                                          TemplateDescriber p_describer,
+                                          TemplateResolver p_resolver,
+                                          String p_pkgPrefix,
+                                          String p_filename,
+                                          String p_absoluteFilename)
         throws IOException,
                ParserException,
                LexerException
@@ -54,7 +74,7 @@ public class TemplateGenerator
         System.out.println(p_filename + " => " + javaFile);
 
         BaseAnalyzer bg =
-            new BaseAnalyzer(p_describer.parseTemplate(p_filename));
+            new BaseAnalyzer(p_describer.parseTemplate(p_absoluteFilename));
 
         pkgDir.mkdirs();
         FileWriter writer = new FileWriter(javaFile);
@@ -75,6 +95,42 @@ public class TemplateGenerator
         }
         writer.close();
     }
+
+
+    public static void generateInterfaces(File p_destDir,
+                                          String p_pkgPrefix,
+                                          String[] p_relativeFilenames,
+                                          String[] p_absoluteFilenames)
+        throws IOException,
+               ParserException,
+               LexerException
+    {
+        if (p_relativeFilenames.length != p_absoluteFilenames.length)
+        {
+            throw new RuntimeException("Arrays of relative and absolute filenames have different length");
+        }
+
+        p_destDir.mkdirs();
+        if (! p_destDir.exists() || ! p_destDir.isDirectory())
+        {
+            throw new IOException("Unable to create destination dir "
+                                  + p_destDir);
+        }
+        
+        TemplateResolver resolver = new TemplateResolver(p_pkgPrefix);
+        TemplateDescriber describer = new TemplateDescriber("");
+        
+        for (int i = 0; i < p_relativeFilenames.length; i++)
+        {
+            generateInterface(p_destDir, 
+                              describer, 
+                              resolver, 
+                              p_pkgPrefix, 
+                              p_relativeFilenames[i],
+                              p_absoluteFilenames[i]);
+        }
+    }
+
 
     public static void main(String [] args)
     {
