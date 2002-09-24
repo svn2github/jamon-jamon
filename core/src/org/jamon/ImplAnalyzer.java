6c419b5f7e0a21c629a9b965a3ea8f6dac1ec6d6
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-09-24 01:58:46 +0000

    Constructor may throw IOException
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@213 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/src/org/jamon/ImplAnalyzer.java
+++ core/src/org/jamon/ImplAnalyzer.java
@@ -1,3 +1,23 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Jamon code, released ??.
+ *
+ * The Initial Developer of the Original Code is Jay Sachs.  Portions
+ * created by Jay Sachs are Copyright (C) 2002 Jay Sachs.  All Rights
+ * Reserved.
+ *
+ * Contributor(s):
+ */
+
 package org.jamon;
 
 import java.io.Writer;
@@ -22,6 +42,7 @@ public class ImplAnalyzer extends BaseAnalyzer
     private final String m_path;
 
     public ImplAnalyzer(String p_templatePath, Start p_start)
+        throws IOException
     {
         m_unitStatements.put(MAIN_UNIT_NAME,new ArrayList());
         m_path = p_templatePath;
