d798df39dea5ae658589ecb187e3a2c652152215
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-09-25 02:47:56 +0000

    Enable invoking templates with simple arguments, required or optional. If required arguments are not specified, they are set to null.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@221 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/src/org/jamon/Invoker.java
+++ core/src/org/jamon/Invoker.java
@@ -23,13 +23,18 @@ package org.jamon;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 
 public class Invoker
 {
     Invoker(String[] args)
         throws UsageException,
+               TemplateArgumentException,
                InvalidTemplateException
     {
         if (args.length == 0)
@@ -38,21 +43,169 @@ public class Invoker
         }
         try
         {
-            StandardTemplateManager manager = new StandardTemplateManager();
-            Class templateClass = Class.forName(args[0]);
-            Constructor con = templateClass.getConstructor
-                ( new Class[] { TemplateManager.class } );
-            m_template = (AbstractTemplateProxy)
-                con.newInstance(new Object[] { manager } );
+            m_templateClass = Class.forName(args[0]);
+            Constructor con = m_templateClass
+                .getConstructor(new Class[] { TemplateManager.class });
             Method setWriterMethod =
-                templateClass.getMethod("setWriter",
-                                        new Class [] { Writer.class } );
+                m_templateClass.getMethod("setWriter",
+                                          new Class [] { Writer.class });
+            Method[] methods = m_templateClass.getMethods();
+            Method render = null;
+            for (int i = 0; i < methods.length; ++i)
+            {
+                if (methods[i].getName().equals("render"))
+                {
+                    render = methods[i];
+                    break;
+                }
+            }
+            if (render == null)
+            {
+                throw new InvalidTemplateException(args[0]);
+            }
+            m_renderMethod = render;
+            m_argMap = new HashMap();
+            for (int i = 1; i < args.length; ++i)
+            {
+                parseArgString(args[i]);
+            }
+
+            m_template = (AbstractTemplateProxy)
+                con.newInstance(new Object[]{ new StandardTemplateManager() });
             setWriterMethod.invoke
                 (m_template,
                  new Object[] { new OutputStreamWriter(System.out) });
 
-            m_renderMethod =
-                templateClass.getMethod("render", new Class [] { } );
+        }
+        catch (Error e)
+        {
+            throw e;
+        }
+        catch (RuntimeException e)
+        {
+            throw e;
+        }
+        catch (UsageException e)
+        {
+            throw e;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            throw new InvalidTemplateException(args[0]);
+        }
+    }
+
+    private void parseArgString(String p_arg)
+        throws UsageException
+    {
+        int i = p_arg.indexOf("=");
+        if (i <= 0)
+        {
+            throw new UsageException();
+        }
+        m_argMap.put(p_arg.substring(0,i),p_arg.substring(i+1));
+    }
+
+    private Object parse(Class p_type, String p_string)
+        throws TemplateArgumentException
+    {
+        try
+        {
+            if (p_string == null)
+            {
+                if (p_type.isPrimitive())
+                {
+                    throw new TemplateArgumentException();
+                }
+                else
+                {
+                return null;
+                }
+            }
+            else if (p_type == String.class)
+            {
+                return p_string;
+            }
+            else if (p_type == Integer.class || p_type == Integer.TYPE)
+            {
+                return Integer.valueOf(p_string);
+            }
+            else if (p_type == Float.class || p_type == Float.TYPE)
+            {
+                return Float.valueOf(p_string);
+            }
+            else if (p_type == Double.class || p_type == Double.TYPE)
+            {
+                return Double.valueOf(p_string);
+            }
+            else if (p_type == Short.class || p_type == Short.TYPE)
+            {
+                return Short.valueOf(p_string);
+            }
+            else if (p_type == Byte.class || p_type == Byte.TYPE)
+            {
+                return Byte.valueOf(p_string);
+            }
+            else
+            {
+                return p_type
+                    .getConstructor(new Class[] { String.class })
+                    .newInstance(new Object[] { p_string });
+            }
+        }
+        catch (Error e)
+        {
+            throw e;
+        }
+        catch (NumberFormatException e)
+        {
+            throw new TemplateArgumentException();
+        }
+        catch (RuntimeException e)
+        {
+            throw e;
+        }
+        catch (Exception e)
+        {
+            throw new TemplateArgumentException();
+        }
+    }
+
+    private final Class m_templateClass;
+    private final AbstractTemplateProxy m_template;
+    private final Method m_renderMethod;
+    private final Map m_argMap;
+
+
+    private void invokeSet(String p_name, String p_value)
+        throws TemplateArgumentException
+    {
+        Method[] methods = m_templateClass.getMethods();
+        String name = "set"
+            + Character.toUpperCase(p_name.charAt(0))
+            + p_name.substring(1);
+        Method setMethod = null;
+        for (int i = 0; i < methods.length; ++i)
+        {
+            if (methods[i].getName().equals(name))
+            {
+                setMethod = methods[i];
+            }
+        }
+        if (setMethod == null)
+        {
+            throw new TemplateArgumentException();
+        }
+        Class[] paramTypes = setMethod.getParameterTypes();
+        if (paramTypes.length != 1)
+        {
+            throw new TemplateArgumentException();
+        }
+        try
+        {
+            setMethod.invoke(m_template,
+                             new Object[] { parse(paramTypes[0], p_value) });
         }
         catch (Error e)
         {
@@ -64,19 +217,32 @@ public class Invoker
         }
         catch (Exception e)
         {
-            throw new InvalidTemplateException(args[0]);
+            e.printStackTrace();
+            throw new TemplateArgumentException();
         }
     }
-
-    private final AbstractTemplateProxy m_template;
-    private final Method m_renderMethod;
 
     void renderTemplate()
         throws IOException, ClassNotFoundException
     {
         try
         {
-            m_renderMethod.invoke(m_template, new Object[] { });
+            Field required = m_templateClass.getField("REQUIRED_ARGS");
+            String[] requiredArgNames = (String[]) required.get(null);
+            Object[] actuals = new Object[requiredArgNames.length];
+            Class[] paramTypes = m_renderMethod.getParameterTypes();
+
+            for (int i = 0; i < requiredArgNames.length; ++i)
+            {
+                actuals[i] = parse(paramTypes[i],
+                                   (String) m_argMap.remove(requiredArgNames[i]));
+            }
+            for (Iterator i = m_argMap.keySet().iterator(); i.hasNext(); /* */)
+            {
+                String name = (String) i.next();
+                invokeSet(name,(String) m_argMap.get(name));
+            }
+            m_renderMethod.invoke(m_template, actuals);
         }
         catch (Error e)
         {
@@ -85,6 +251,10 @@ public class Invoker
         catch (RuntimeException e)
         {
             throw e;
+        }
+        catch (TemplateArgumentException e)
+        {
+            displayError("supplied arguments are not valid");
         }
         catch (Exception e)
         {
@@ -105,7 +275,7 @@ public class Invoker
         }
         catch (UsageException e)
         {
-            displayError(e.usage());
+            displayError("Usage: " + e.usage());
         }
         catch (InvalidTemplateException e)
         {
@@ -125,6 +295,11 @@ public class Invoker
             super(p_templateName
                   + " does not appear to be a valid template class");
         }
+    }
+
+    private static class TemplateArgumentException
+        extends Exception
+    {
     }
 
 
