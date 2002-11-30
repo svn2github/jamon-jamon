4f61791a64005ef5e51901c95523858c0fed671a
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-11-30 03:45:26 +0000

    80 columns, please.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@355 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/src/org/jamon/StandardTemplateManager.java
+++ core/src/org/jamon/StandardTemplateManager.java
@@ -321,10 +321,11 @@ public class StandardTemplateManager
             }
             if (m_workDir == null)
             {
-                File workDir = new File(System.getProperty("java.io.tmpdir"),
-                                        "jamon"
-                                        + (new java.util.Random().nextInt(100000000))
-                                        + ".tmp");
+                File workDir =
+                    new File(System.getProperty("java.io.tmpdir"),
+                             "jamon"
+                             + (new java.util.Random().nextInt(100000000))
+                             + ".tmp");
                 m_workDir = workDir.getCanonicalPath();
                 workDir.mkdirs();
                 workDir.deleteOnExit();
