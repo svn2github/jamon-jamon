42685318f8415c3cd072f5e1bcd1f93622d1ad69
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-11-30 03:23:30 +0000

    The default escaping is DEFAULT.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@349 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/src/org/jamon/ImplAnalyzer.java
+++ core/src/org/jamon/ImplAnalyzer.java
@@ -277,7 +277,7 @@ public class ImplAnalyzer extends BaseAnalyzer
                          ("\""
                           + javaEscape(newlineEscape(m_current.toString()))
                           + "\"",
-                          EscapingDirective.NONE));
+                          EscapingDirective.DEFAULT));
             m_current = new StringBuffer();
         }
     }
