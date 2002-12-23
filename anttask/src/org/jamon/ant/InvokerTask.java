24fd5c5d21c7612605db7081fc015ac4cee6c8f5
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2002-12-23 02:38:27 +0000

    New Ant Task for handling reflective template invocations.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@435 6afd1a24-8710-0410-9bc4-c69fca332c4c

---
+++ anttask/src/org/jamon/ant/InvokerTask.java
@@ -0,0 +1,130 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Jamon code, released October, 2002.
+ *
+ * The Initial Developer of the Original Code is Jay Sachs.  Portions
+ * created by Jay Sachs are Copyright (C) 2002 Jay Sachs.  All Rights
+ * Reserved.
+ *
+ * Contributor(s):
+ */
+
+package org.jamon.ant;
+
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.HashMap;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Path;
+
+import org.jamon.Invoker;
+import org.jamon.StandardTemplateManager;
+
+/**
+ * Ant task to reflectively invoke templates.
+ **/
+
+public class InvokerTask
+    extends Task
+{
+    public InvokerTask()
+    {
+        m_templateManager = new StandardTemplateManager();
+    }
+
+    public void execute()
+        throws BuildException
+    {
+        try
+        {
+            new Invoker(m_templateManager,m_path)
+                .render(m_output == null
+                        ? new OutputStreamWriter(System.out)
+                        : new FileWriter(m_output),
+                        m_args);
+        }
+        catch (Invoker.InvalidTemplateException e)
+        {
+            e.printStackTrace(System.err);
+        }
+        catch (IOException e)
+        {
+            throw new BuildException(e);
+        }
+    }
+
+    public void setClasspath(Path p_classpath)
+        throws IOException
+    {
+        String[] paths = p_classpath.list();
+        URL[] urls = new URL[paths.length];
+        for (int i = 0; i < urls.length; ++i)
+        {
+            urls[i] = new URL("file",null, paths[i]);
+        }
+        m_templateManager
+            .setClassLoader(new URLClassLoader(urls,
+                                               getClass().getClassLoader()));
+        m_templateManager.setClasspath(p_classpath.toString());
+    }
+
+    public void setWorkDir(File p_workDir)
+    {
+        m_templateManager.setWorkDir(p_workDir.getAbsolutePath());
+    }
+
+    public void setSourceDir(File p_sourceDir)
+    {
+        m_templateManager.setSourceDir(p_sourceDir.getAbsolutePath());
+    }
+
+    public void setOutput(File p_output)
+        throws IOException
+    {
+        m_output = p_output;
+    }
+
+    public void setTemplate(String p_path)
+    {
+        m_path = p_path;
+    }
+
+    private final StandardTemplateManager m_templateManager;
+    private String m_path;
+    private HashMap m_args = new HashMap();
+    private File m_output;
+
+    public void addConfiguredArg(Arg p_arg)
+    {
+        m_args.put(p_arg.m_name, p_arg.m_value);
+    }
+
+    public static class Arg
+    {
+        public void setName(String p_name)
+        {
+            m_name = p_name;
+        }
+        public void setValue(String p_value)
+        {
+            m_value = p_value;
+        }
+        private String m_name;
+        private String m_value;
+    }
+}
