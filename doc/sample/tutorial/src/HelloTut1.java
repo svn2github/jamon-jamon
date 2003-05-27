5b8f6993e238ff63c56649dc277e80f6bf17632b
trees <trees@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2003-05-27 18:43:36 +0000

    Moved templates and java sources used in the tutorial to a separate directory.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@1048 6afd1a24-8710-0410-9bc4-c69fca332c4c

---
+++ doc/sample/tutorial/src/HelloTut1.java
@@ -0,0 +1,14 @@
+import java.io.OutputStreamWriter;
+import org.jamon.StandardTemplateManager;
+
+public class HelloTut1 {
+  public static void main(String[] argv) {
+    try {
+      StandardTemplateManager mgr = new StandardTemplateManager();
+      new HelloTemplate(mgr).render(new OutputStreamWriter(System.out));
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
