a51c25594a6504e27c2f2b666a94d944af2b3f43
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2003-06-18 18:11:01 +0000

    Simplify now that a StandardTemplateManager will be constructed automatically for us.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@1208 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- doc/sample/overview/src/JamonQs.java
+++ doc/sample/overview/src/JamonQs.java
@@ -1,16 +1,10 @@
 import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.Date;
-import org.jamon.StandardTemplateManager;
 
 public class JamonQs {
-  public static void main(String[] argv) {
-    try {
-      StandardTemplateManager mgr = new StandardTemplateManager();
-      QsTemp template = new QsTemp(mgr);
-      template.render(new OutputStreamWriter(System.out), new Date(0),argv);
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
+  public static void main(String[] argv) throws IOException {
+    QsTemp template = new QsTemp();
+    template.render(new OutputStreamWriter(System.out), new Date(0),argv);
   }
 }
