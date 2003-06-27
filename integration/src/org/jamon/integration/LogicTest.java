2cc69e15ef333ee018678f00972b3d3d890f17cb
ian <ian@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2003-06-27 19:47:40 +0000

    2 new standard library templates, If and IfElse
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@1277 6afd1a24-8710-0410-9bc4-c69fca332c4c

---
+++ integration/src/org/jamon/integration/LogicTest.java
@@ -0,0 +1,60 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Jamon code, released February, 2003.
+ *
+ * The Initial Developer of the Original Code is Ian Robertson.  Portions
+ * created by Ian Robertson are Copyright (C) 2003 Ian Robertson.  All Rights
+ * Reserved.
+ *
+ * Contributor(s):
+ */
+
+package org.jamon.integration;
+
+import test.jamon.IfTest;
+import test.jamon.IfElseTest;
+
+/**
+ * Test Jamon's standard library logic templates.
+ **/
+
+public class LogicTest
+    extends TestBase
+{
+    public void testIfTrue()
+        throws Exception
+    {
+        new IfTest().render(getWriter(), true);
+        checkOutput("then");
+    }
+
+    public void testIfFalse()
+        throws Exception
+    {
+        new IfTest().render(getWriter(), false);
+        checkOutput("");
+    }
+
+    public void testIfElseTrue()
+        throws Exception
+    {
+        new IfElseTest().render(getWriter(), true);
+        checkOutput("then");
+    }
+
+    public void testIfElseFalse()
+        throws Exception
+    {
+        new IfElseTest().render(getWriter(), false);
+        checkOutput("else");
+    }
+}
