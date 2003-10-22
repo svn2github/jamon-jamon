4a1a723d435318df557cc94b5f926ccc3baea682
jay <jay@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2003-10-22 17:38:10 +0000

    Begin integration testing encoded templates.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@1426 6afd1a24-8710-0410-9bc4-c69fca332c4c

---
+++ integration/src/org/jamon/integration/EncodingsTest.java
@@ -0,0 +1,37 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Jamon code, released February, 2003.
+ *
+ * The Initial Developer of the Original Code is Jay Sachs.  Portions
+ * created by Jay Sachs are Copyright (C) 2003 Jay Sachs.  All Rights
+ * Reserved.
+ *
+ * Contributor(s):
+ */
+
+package org.jamon.integration;
+
+/**
+ * Test Jamon's encoding mechanisms.
+ **/
+
+public class EncodingsTest
+    extends TestBase
+{
+    public void testUtf8()
+        throws Exception
+    {
+        new test.jamon.Utf8().render(getWriter());
+        checkOutput("Onc\u00e9\u00f2\u00e1\n\u00e9\u00c1\u00f3\u00e9\n");
+    }
+
+}
