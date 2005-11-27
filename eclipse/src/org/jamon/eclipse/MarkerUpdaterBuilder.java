b8262a5f97325106b35c5993090702f76789cdd8
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2005-11-27 23:56:43 +0000

    Imports are fun
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@1770 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- eclipse/src/org/jamon/eclipse/MarkerUpdaterBuilder.java
+++ eclipse/src/org/jamon/eclipse/MarkerUpdaterBuilder.java
@@ -11,39 +11,40 @@ import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public class MarkerUpdaterBuilder extends IncrementalProjectBuilder {
 
 	@Override
-    protected IProject[] build(int kind, Map args, IProgressMonitor monitor) 
+    protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
     {
-        //FIXME - there has to be a better way.  We don't want really want a 
+        //FIXME - there has to be a better way.  We don't want really want a
         // builder here, because it's not when the impl and proxies change, it's
-        // when they are marked that we need to handle things.  A resource 
+        // when they are marked that we need to handle things.  A resource
         // change listener is what we need.  But a builder seems to be the only
         // way to instantiate the listener.  Moreover, there needs to be a way
         // to remove the listener if the project is closed, or if the jamon
         // nature is removed.
-        
+
         if (m_javaMarkerListener == null)
         {
             m_javaMarkerListener = new JavaMarkerListener(
                 JamonNature.templateSourceFolder(getProject()),
                 JamonNature.templateOutputFolder(getProject()));
         }
-        org.eclipse.core.resources.ResourcesPlugin.getWorkspace()
+        ResourcesPlugin.getWorkspace()
             .addResourceChangeListener(
                 m_javaMarkerListener, IResourceChangeEvent.POST_BUILD);
         return null;
 	}
-	
+
 	private static String builderId() {
 		return JamonProjectPlugin.getDefault().pluginId() + ".markerUpdater";
 	}
 
-	
+
 	public static void addToProject(IProject p_project) throws CoreException {
 		IProjectDescription description = p_project.getDescription();
 		ArrayList<ICommand> cmds = new ArrayList<ICommand>();
