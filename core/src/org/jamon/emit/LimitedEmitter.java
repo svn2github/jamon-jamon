715383aeb8dc04bfe3491829814e64ca54f49b94
jaysachs <jaysachs@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2006-09-17 17:48:00 +0000

    Eliminate static access warnings in generated templates.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@1910 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/src/org/jamon/emit/LimitedEmitter.java
+++ core/src/org/jamon/emit/LimitedEmitter.java
@@ -21,11 +21,50 @@
 package org.jamon.emit;
 
 public final class LimitedEmitter
-    extends BaseEmitter
 {
     private LimitedEmitter()
     {
         // non instantiable
+    }
+
+    public static String valueOf(int p_int)
+    {
+        return StandardEmitter.valueOf(p_int);
+    }
+
+    public static String valueOf(double p_double)
+    {
+        return StandardEmitter.valueOf(p_double);
+    }
+
+    public static String valueOf(char p_char)
+    {
+        return StandardEmitter.valueOf(p_char);
+    }
+
+    public static String valueOf(boolean p_bool)
+    {
+        return StandardEmitter.valueOf(p_bool);
+    }
+
+    public static String valueOf(float p_float)
+    {
+        return StandardEmitter.valueOf(p_float);
+    }
+
+    public static String valueOf(short p_short)
+    {
+        return StandardEmitter.valueOf(p_short);
+    }
+
+    public static String valueOf(long p_long)
+    {
+        return StandardEmitter.valueOf(p_long);
+    }
+
+    public static String valueOf(byte p_byte)
+    {
+        return StandardEmitter.valueOf(p_byte);
     }
 
     public static String valueOf(Integer p_value)
