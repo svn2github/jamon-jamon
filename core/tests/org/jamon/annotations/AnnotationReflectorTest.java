72e9db28bc95b8e17aacfffebe6490b93f5a5ec5
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-04-15 01:50:12 +0000

    import cleaning
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@2171 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- core/tests/org/jamon/annotations/AnnotationReflectorTest.java
+++ core/tests/org/jamon/annotations/AnnotationReflectorTest.java
@@ -6,7 +6,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
