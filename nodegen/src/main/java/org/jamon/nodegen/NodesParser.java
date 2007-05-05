487b35178f99eab6248c3cd92bccc9b69ffef077
jaysachs <jaysachs@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-05-05 20:02:56 +0000

    Organize imports
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@2252 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- nodegen/src/main/java/org/jamon/nodegen/NodesParser.java
+++ nodegen/src/main/java/org/jamon/nodegen/NodesParser.java
@@ -1,11 +1,7 @@
 package org.jamon.nodegen;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.LineNumberReader;
-import java.io.PrintWriter;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
