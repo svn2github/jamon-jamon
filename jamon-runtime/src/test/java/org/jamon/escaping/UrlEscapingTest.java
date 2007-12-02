1e5402cd5062165ddae7081a37b3911d04e79b24
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2007-12-02 19:21:26 +0000

    Simplify test; let Sun test their code.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@2517 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- jamon-runtime/src/test/java/org/jamon/escaping/UrlEscapingTest.java
+++ jamon-runtime/src/test/java/org/jamon/escaping/UrlEscapingTest.java
@@ -22,100 +22,16 @@ package org.jamon.escaping;
 
 import java.io.Writer;
 import java.io.StringWriter;
-import java.io.IOException;
 
 import junit.framework.TestCase;
 
 public class UrlEscapingTest
     extends TestCase
 {
-    public void testAscii() throws IOException
-    {
-        for (char c = 0; c <= 0x7f; c++)
-        {
-            if( Character.isLetterOrDigit(c)
-                || c == '.'
-                || c == '-'
-                || c == '*'
-                || c == '_')
-            {
-                check(charToString(c), charToString(c));
-            }
-            else if (c == ' ')
-            {
-                check(charToString(c), "+");
-            }
-            else {
-                check(charToString(c),
-                      "%"
-                      + ((c < 0x10) ? "0" : "")
-                      + Integer.toHexString(c).toUpperCase());
-            }
-        }
-    }
-
-    private static String charToString(char c)
-    {
-        return new String(new char[] {c});
-    }
-
-    public void testNonAscii() throws IOException
-    {
-        check("\u0080", "%C2%80");
-        check("\u0100", "%C4%80");
-        check("\u0200", "%C8%80");
-        check("\u0400", "%D0%80");
-        check("\u0800", "%E0%A0%80");
-        check("\u1000", "%E1%80%80");
-        check("\u2000", "%E2%80%80");
-        check("\u4000", "%E4%80%80");
-        check("\u8000", "%E8%80%80");
-    }
-
-    public void testUtf16SurrogatesInLine() throws IOException
-    {
-        check("q\ud800\udc00r", "q%F0%90%80%80r");
-    }
-
-
-    public void testUtf16SurrogateBits() throws IOException
-    {
-        check("\ud800\udc00", "%F0%90%80%80");
-        check("\ud800\udc01", "%F0%90%80%81");
-        check("\ud800\udc02", "%F0%90%80%82");
-        check("\ud800\udc04", "%F0%90%80%84");
-        check("\ud800\udc08", "%F0%90%80%88");
-        check("\ud800\udc10", "%F0%90%80%90");
-        check("\ud800\udc20", "%F0%90%80%A0");
-        check("\ud800\udc40", "%F0%90%81%80");
-        check("\ud800\udc80", "%F0%90%82%80");
-        check("\ud800\udd00", "%F0%90%84%80");
-        check("\ud800\ude00", "%F0%90%88%80");
-        check("\ud801\udc00", "%F0%90%90%80");
-        check("\ud802\udc00", "%F0%90%A0%80");
-        check("\ud804\udc00", "%F0%91%80%80");
-        check("\ud808\udc00", "%F0%92%80%80");
-        check("\ud810\udc00", "%F0%94%80%80");
-        check("\ud820\udc00", "%F0%98%80%80");
-        check("\ud840\udc00", "%F0%A0%80%80");
-        check("\ud880\udc00", "%F0%B0%80%80");
-        check("\ud900\udc00", "%F1%90%80%80");
-        check("\udA00\udc00", "%F2%90%80%80");
-    }
-
-    public void testUtf16BadSurrogates() throws IOException
-    {
-        check("\udf02\u0000", "%3F%00"); // starting with a low surrogate
-        check("\udf02", "%3F"); // starting and ending with a low surrogate
-        check("\ud800\u0000", "%3F%00"); // bad low surrogate
-        check("\ud800", ""); // ending with a high surrogate
-    }
-
-    private void check(String p_text, String p_expected)
-        throws IOException
+    public void testEncoding() throws Exception
     {
         Writer writer = new StringWriter();
-        Escaping.URL.write(p_text, writer);
-        assertEquals((Object) p_expected, writer.toString());
+        Escaping.URL.write("a+b c%d", writer);
+        assertEquals((Object) "a%2Bb+c%25d", writer.toString());
     }
 }
