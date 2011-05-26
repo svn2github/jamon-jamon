0acafe16f7195b8afb0292d81fa73089e719a043
iroberts <iroberts@6afd1a24-8710-0410-9bc4-c69fca332c4c>
2011-05-26 20:06:10 +0000

    Test replacing a simple template with a template that is inherited.
    git-svn-id: svn://svn.code.sf.net/p/jamon/code/trunk/jamon@2761 6afd1a24-8710-0410-9bc4-c69fca332c4c

--- jamon-integration/jamon-integration-tests/src/test/java/org/jamon/integration/ReplacementTest.java
+++ jamon-integration/jamon-integration-tests/src/test/java/org/jamon/integration/ReplacementTest.java
@@ -2,13 +2,18 @@ package org.jamon.integration;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Map;
+
 import org.jamon.AbstractReplacingTemplateManager;
 import org.jamon.AbstractTemplateProxy;
 import org.jamon.TemplateManager;
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableMap;
+
 import test.jamon.replacement.Api;
 import test.jamon.replacement.ApiChildReplacementCaller;
+import test.jamon.replacement.ApiReplacementChild;
 import test.jamon.replacement.ApiWithFargsCaller;
 import test.jamon.replacement.GenericApiCaller;
 
@@ -31,10 +36,38 @@ public class ReplacementTest
         }
     };
 
+    private final static class ReplacingTemplateManager
+    extends AbstractReplacingTemplateManager
+    {
+        private final Map<Class<? extends AbstractTemplateProxy>, Class<? extends AbstractTemplateProxy>> m_replacements;
+
+        public ReplacingTemplateManager(
+            Class<? extends AbstractTemplateProxy> p_replaced,
+            Class<? extends AbstractTemplateProxy> p_replacement) {
+            this(ImmutableMap.<Class<? extends AbstractTemplateProxy>,
+                               Class<? extends AbstractTemplateProxy>>of(
+                                   p_replaced, p_replacement));
+        }
+
+        private ReplacingTemplateManager(
+            Map<Class<? extends AbstractTemplateProxy>, Class<? extends AbstractTemplateProxy>> p_replacements)
+        {
+            m_replacements = p_replacements;
+        }
+
+        @Override
+        protected Class<? extends AbstractTemplateProxy> findReplacement(Class<?> p_proxyClass)
+        {
+            return m_replacements.get(p_proxyClass);
+        }
+
+
+    }
+
     @Test
     public void testSimpleReplacement() throws Exception
     {
-        assertEquals("Implementor: 3 4", new Api(TEMPLATE_MANAGER).makeRenderer(3, 4).asString());
+        assertEquals("Replacement: 3 4", new Api(TEMPLATE_MANAGER).makeRenderer(3, 4).asString());
     }
 
     @Test
@@ -58,5 +91,13 @@ public class ReplacementTest
         assertEquals(
             "s1: hello\ns2: t\ni1: 1\ni2: 5\nfragment f\nfragment g",
             new ApiChildReplacementCaller(TEMPLATE_MANAGER).makeRenderer().asString());
+    }
+
+    @Test
+    public void testReplacementOfTemplateWithChildTemplate() throws Exception {
+        assertEquals(
+            "Parent: 3 Child: 4",
+            new Api(new ReplacingTemplateManager(Api.class, ApiReplacementChild.class))
+                .makeRenderer(3, 4).asString());
     }
 }
